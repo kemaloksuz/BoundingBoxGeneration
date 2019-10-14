244331dc0691358ded4efe63df2cde1eea393e9d
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2019-10-14 22:56:29 +0300

    Update max_softiou_assigner_conditional.py

--- mmdet/core/bbox/assigners/max_softiou_assigner_conditional.py
+++ mmdet/core/bbox/assigners/max_softiou_assigner_conditional.py
@@ -76,10 +76,10 @@ class MaxSoftIoUConditionalAssigner(BaseAssigner):
         bboxes = bboxes[:, :4]
         overlaps = bbox_overlaps(gt_bboxes, bboxes)
         segm_ious = segm_iou(gt_masks, gt_bboxes, bboxes, overlaps, 0.45) 
-        FP_indices = (segm_ious < 0.6)* (overlaps > 0.5)
-        #FN_indices = (segm_ious > 0.75) * (overlaps < 0.5) * (overlaps > 0.45)
-        overlaps[FP_indices]=0.450
-        #overlaps[FN_indices]=0.501
+        #FP_indices = (segm_ious < 0.6)* (overlaps > 0.5)
+        FN_indices = (segm_ious > 0.75) * (overlaps < 0.5) * (overlaps > 0.45)
+        #overlaps[FP_indices]=0.450
+        overlaps[FN_indices]=0.501
 #        overlaps=bbox_ious*overlaps
         
         if (self.ignore_iof_thr > 0) and (gt_bboxes_ignore is not None) and (
