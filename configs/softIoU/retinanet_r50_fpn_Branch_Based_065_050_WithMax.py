fc84c8380b40fb29aa123348b56eb18fc49972c0
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2019-10-15 18:07:02 +0300

    Update retinanet_r50_fpn_Branch_Based_065_050_WithMax.py

--- configs/softIoU/retinanet_r50_fpn_Branch_Based_065_050_WithMax.py
+++ configs/softIoU/retinanet_r50_fpn_Branch_Based_065_050_WithMax.py
@@ -130,7 +130,7 @@ total_epochs = 12
 device_ids = range(2)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/retinanet_r50_fpn_softIoU_065_065_WithMax'
+work_dir = './work_dirs/retinanet_r50_fpn_Branch_Based_065_050_WithMax'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
