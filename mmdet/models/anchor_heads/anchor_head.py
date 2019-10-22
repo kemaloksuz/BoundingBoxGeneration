d0dab6c06b7544e580581491cc794bfae398c1e5
cancam <bariscancam@gmail.com>
2019-10-22 16:56:08 +0300

    minor bug fixes

--- mmdet/models/anchor_heads/anchor_head.py
+++ mmdet/models/anchor_heads/anchor_head.py
@@ -74,7 +74,7 @@ class AnchorHead(nn.Module):
 
         self.num_anchors = len(self.anchor_ratios) * len(self.anchor_scales)
         self._init_layers()
-        self.filename = './testing.dat'
+        self.filename = './testing_baseline.dat'
     def _init_layers(self):
         self.conv_cls = nn.Conv2d(self.feat_channels,
                                   self.num_anchors * self.cls_out_channels, 1)
@@ -163,14 +163,15 @@ class AnchorHead(nn.Module):
                 det_labels[det_labels<0] = 0
                 det_cls_score=cls_score[idx,det_labels].sigmoid().cpu().numpy()     
                 det_cls_loss = loss_cls[idx, :].sum(dim=1).cpu().numpy()
-                det_cls_score_max = cls_score[idx, :].max(dim=1).cpu().numpy()
+                #pdb.set_trace()
+                det_cls_score_max = cls_score[idx, :].max(dim=1)[0].cpu().numpy()
                 if loss_bbox.dim()>0:
                     det_iou=loss_bbox[idx].cpu().numpy()
                 else:
                     det_iou=torch.cuda.FloatTensor(idx.size()[0]).fill_(0).cpu().numpy()
                 anchor_iou=IoUs[idx].cpu().numpy()
                 anchor_segmrate=softIoUs[idx].cpu().numpy()
-                tuples=np.concatenate((np.expand_dims(anchor_iou,1),np.expand_dims(anchor_segmrate,1),np.expand_dims(det_iou,1), np.expand_dims(det_cls_score,1), np.expand_dims(det_cls_loss,1),, np.expand_dims(det_cls_score_max,1)),axis=1)
+                tuples=np.concatenate((np.expand_dims(anchor_iou,1),np.expand_dims(anchor_segmrate,1),np.expand_dims(det_iou,1), np.expand_dims(det_cls_score,1), np.expand_dims(det_cls_loss,1), np.expand_dims(det_cls_score_max,1)),axis=1)
                 f = open(self.filename, "ab")
                 np.savetxt(f, tuples)            
 #        pdb.set_trace()
