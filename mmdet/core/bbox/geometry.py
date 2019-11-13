63e43a94868f641c04c4a803672eafee84b4fba0
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2019-11-13 16:48:27 +0200

    Update geometry.py

--- mmdet/core/bbox/geometry.py
+++ mmdet/core/bbox/geometry.py
@@ -62,7 +62,21 @@ def bbox_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False):
 
     return ious
 
-def bbox_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False):
+def integral_image_fetch(mask,bboxes):
+    #import pdb
+    #pdb.set_trace()
+    bboxes[:,[2,3]]+=1
+    #Create indices
+    TLx=bboxes[:,0].tolist()
+    TLy=bboxes[:,1].tolist()
+    BRx=bboxes[:,2].tolist()
+    BRy=bboxes[:,3].tolist()
+    area=mask[BRy,BRx]+mask[TLy,TLx]-mask[TLy,BRx]-mask[BRy,TLx]
+    return area
+
+def normalize_det(det_boxes, gt_bboxes, map_size):
+
+def saliency_aware_bbox_overlaps(bboxes1, bboxes2, gt_saliency_maps, b=0, map_size=224):
     """Calculate overlap between two set of bboxes.
 
     If ``is_aligned`` is ``False``, then calculate the ious between each bbox
@@ -79,46 +93,53 @@ def bbox_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False):
     Returns:
         ious(Tensor): shape (m, n) if is_aligned == False else shape (m, 1)
     """
-
-    assert mode in ['iou', 'iof']
-
     rows = bboxes1.size(0)
     cols = bboxes2.size(0)
-    if is_aligned:
-        assert rows == cols
 
-    if rows * cols == 0:
-        return bboxes1.new(rows, 1) if is_aligned else bboxes1.new(rows, cols)
+    with torch.no_grad():
+        saliency_aware_intersection=bboxes1.data.new_zeros(rows,cols)
+        normalized_detections=normalize_det(bboxes2, bboxes1, map_size)
+        normalized_gts=
+        lt = torch.max(normalized_gts[:, None, :2], normalized_detections[:, :2])  # [rows, cols, 2]
+        rb = torch.min(normalized_gts[:, None, 2:], normalized_detections[:, 2:])  # [rows, cols, 2]        
+        intersected_regions=torch.concat([lt,rb], axis=2)
+        for i in range(rows):
+            saliency_aware_overlap=integral_image_fetch(gt_saliency_maps[i], intersected_regions[i,:,:])
+            area_detections=(normalized_detections[:, 2] - normalized_detections[:, 0] + 1) * (
+            normalized_detections[:, 3] - normalized_detections[:, 1] + 1)
+            ious = saliency_aware_overlap / ((map_size*map_size) + area2 - saliency_aware_overlap)
 
-    if is_aligned:
-        lt = torch.max(bboxes1[:, :2], bboxes2[:, :2])  # [rows, 2]
-        rb = torch.min(bboxes1[:, 2:], bboxes2[:, 2:])  # [rows, 2]
+        if plot:
+            import matplotlib.pyplot as plt
+            from matplotlib import patches as patch
+            import random
+            larger_ind=overlaps>min_overlap
+            nonzero_iou_ind=torch.nonzero(larger_ind)
+            #gt_mask_size=torch.sum(gt_masks,dim=[1,2]).type(torch.cuda.FloatTensor)
+            #end1 = time.time()
+            #bboxes=bboxes.type(torch.cuda.IntTensor) 
+            bboxes=torch.clamp(bboxes, min=0)
+            bboxes[:,[0,2]]=torch.clamp(bboxes[:,[0,2]], max=image_w-1)
+            bboxes[:,[1,3]]=torch.clamp(bboxes[:,[1,3]], max=image_h-1)
 
-        wh = (rb - lt + 1).clamp(min=0)  # [rows, 2]
-        overlap = wh[:, 0] * wh[:, 1]
-        area1 = (bboxes1[:, 2] - bboxes1[:, 0] + 1) * (
-            bboxes1[:, 3] - bboxes1[:, 1] + 1)
+            no=random.randint(0,nonzero_iou_ind.shape[0])
+            pltgt,pltanc=nonzero_iou_ind[no]
+           # print(pltgt,pltanc)
+            fig, ax = plt.subplots(1)
+            ax.imshow(gt_masks[pltgt].cpu().numpy())
 
-        if mode == 'iou':
-            area2 = (bboxes2[:, 2] - bboxes2[:, 0] + 1) * (
-                bboxes2[:, 3] - bboxes2[:, 1] + 1)
-            ious = overlap / (area1 + area2 - overlap)
-        else:
-            ious = overlap / area1
-    else:
-        lt = torch.max(bboxes1[:, None, :2], bboxes2[:, :2])  # [rows, cols, 2]
-        rb = torch.min(bboxes1[:, None, 2:], bboxes2[:, 2:])  # [rows, cols, 2]
+            tempRect=patch.Rectangle((bboxes[pltanc,0],bboxes[pltanc,1]), bboxes[pltanc,2]-bboxes[pltanc,0], bboxes[pltanc,3]-bboxes[pltanc,1],linewidth=3,edgecolor='r',facecolor='none')
+            ax.add_patch(tempRect) 
+            fntsize=14
+            tempRect=patch.Rectangle((gt_bboxes[pltgt,0],gt_bboxes[pltgt,1]), gt_bboxes[pltgt,2]-gt_bboxes[pltgt,0], gt_bboxes[pltgt,3]-gt_bboxes[pltgt,1],linewidth=3,edgecolor='g',facecolor='none')
+            ax.add_patch(tempRect)        
 
-        wh = (rb - lt + 1).clamp(min=0)  # [rows, cols, 2]
-        overlap = wh[:, :, 0] * wh[:, :, 1]
-        area1 = (bboxes1[:, 2] - bboxes1[:, 0] + 1) * (
-            bboxes1[:, 3] - bboxes1[:, 1] + 1)
-
-        if mode == 'iou':
-            area2 = (bboxes2[:, 2] - bboxes2[:, 0] + 1) * (
-                bboxes2[:, 3] - bboxes2[:, 1] + 1)
-            ious = overlap / (area1[:, None] + area2 - overlap)
-        else:
-            ious = overlap / (area1[:, None])
+            ax.tick_params(labelsize=fntsize)      
+            plt.xlabel('x', fontsize=fntsize)
+            plt.ylabel('y', fontsize=fntsize)
+            ax.text(0, 0, "iou= "+np.array2string(overlaps[pltgt,pltanc].cpu().numpy())+", "+\
+                "\n segm_rate="+np.array2string(segm_ious[pltgt,pltanc].cpu().numpy())+", "+\
+                "\n soft_iou="+np.array2string(soft_ious[pltgt,pltanc].cpu().numpy()), fontsize=12)
+            plt.show()
 
     return ious    
