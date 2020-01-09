126c057f3d7a167eff7ad4e86fdfa936f275d68b
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-01-09 20:25:49 +0200

    smooth l1 reverted

--- mmdet/models/losses/smooth_l1_loss.py
+++ mmdet/models/losses/smooth_l1_loss.py
@@ -7,13 +7,11 @@ from .utils import weighted_loss
 
 @weighted_loss
 def smooth_l1_loss(pred, target, beta=1.0):
+    assert beta > 0
     assert pred.size() == target.size() and target.numel() > 0
     diff = torch.abs(pred - target)
-    if beta>0:
-        loss = torch.where(diff < beta, 0.5 * diff * diff / beta,
+    loss = torch.where(diff < beta, 0.5 * diff * diff / beta,
                        diff - 0.5 * beta)
-    else:
-        loss = diff
     return loss
 
 
