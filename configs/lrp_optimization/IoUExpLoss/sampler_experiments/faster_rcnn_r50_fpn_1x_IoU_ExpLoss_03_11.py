3c5ab03a4a5a27e18917de25cb769e5615b7f363
cancam <bariscancam@gmail.com>
2020-01-11 14:14:28 +0300

    kemal

--- configs/lrp_optimization/IoUExpLoss/sampler_experiments/faster_rcnn_r50_fpn_1x_IoU_ExpLoss_03_11.py
+++ configs/lrp_optimization/IoUExpLoss/sampler_experiments/faster_rcnn_r50_fpn_1x_IoU_ExpLoss_03_11.py
@@ -166,7 +166,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 12
+total_epochs = 2
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/lrp_optimization/faster_rcnn_r50_fpn_1x_1-IoU_ExpLoss03_11'
