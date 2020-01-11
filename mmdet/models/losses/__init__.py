0527ea947634fe3023c0a3b0d9b10928e9c61fd1
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-01-12 01:57:46 +0200

    l2

--- mmdet/models/losses/__init__.py
+++ mmdet/models/losses/__init__.py
@@ -8,12 +8,13 @@ from .iou_loss import BoundedIoULoss, IoULoss, bounded_iou_loss, iou_loss
 from .mse_loss import MSELoss, mse_loss
 from .smooth_l1_loss import SmoothL1Loss, smooth_l1_loss
 from .l1_loss import L1Loss, l1_loss
+from .l2_loss import L2Loss, l2_loss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'sigmoid_focal_loss',
-    'FocalLoss', 'smooth_l1_loss', 'SmoothL1Loss', 'balanced_l1_loss', 'l1_loss', 'L1Loss',
+    'FocalLoss', 'smooth_l1_loss', 'SmoothL1Loss', 'balanced_l1_loss', 'l1_loss', 'L1Loss', 'l2_loss', 'L2Loss',
     'BalancedL1Loss', 'mse_loss', 'MSELoss', 'iou_loss', 'bounded_iou_loss',
     'IoULoss', 'BoundedIoULoss', 'GHMC', 'GHMR', 'reduce_loss',
     'weight_reduce_loss', 'weighted_loss'
