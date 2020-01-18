ef0e45124f8e9669462a6c9c5952db99c1eca63f
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-01-18 21:43:34 +0200

    Update retinanet_r50_fpn_MaskAwareIoU_05_05_WithMax_GammaMask1.py

--- configs/softIoU/retinanet_r50_fpn_MaskAwareIoU_05_05_WithMax_GammaMask1.py
+++ configs/softIoU/retinanet_r50_fpn_MaskAwareIoU_05_05_WithMax_GammaMask1.py
@@ -28,7 +28,7 @@ model = dict(
         anchor_strides=[8, 16, 32, 64, 128],
         target_means=[.0, .0, .0, .0],
         target_stds=[1.0, 1.0, 1.0, 1.0],
-        filename='./work_dirs/retinanet_r50_fpn_MaskAwareIoU_05_05_WithMax_GammaMask1/bg_fg.txt',
+        filename='./work_dirs/ExtractBandClass/bg_fg.txt',
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
@@ -125,7 +125,7 @@ log_config = dict(
 total_epochs = 1
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/retinanet_r50_fpn_MaskAwareIoU_05_05_WithMax_GammaMask1'
+work_dir = './work_dirs/ExtractBandClass'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
