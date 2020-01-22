ab6af93fb8eb2ff264db78a72c2b204dfff8052e
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-01-22 17:39:18 +0200

    configs

--- configs/softIoU/MaskAwareIoU/COCO/OneStage/ssd300_coco_G025.py
+++ configs/softIoU/MaskAwareIoU/COCO/OneStage/ssd300_coco_G025.py
@@ -129,7 +129,7 @@ log_config = dict(
 total_epochs = 24
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/ssd300_MaskGamma025'
+work_dir = './work_dirs/ssd300_coco_G025'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
--- configs/softIoU/MaskAwareIoU/FasterRCNN/faster_rcnn_r101_fpn.py
+++ configs/softIoU/MaskAwareIoU/FasterRCNN/faster_rcnn_r101_fpn.py
@@ -168,7 +168,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_r101_fpn_Baseline'
+work_dir = './work_dirs/faster_rcnn_r101_fpn'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
