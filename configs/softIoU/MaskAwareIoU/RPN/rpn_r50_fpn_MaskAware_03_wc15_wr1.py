649913074178be62cb1949e8dad739420ab9e665
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-02-04 22:11:57 +0200

    rpn configs

--- configs/softIoU/MaskAwareIoU/RPN/rpn_r50_fpn_MaskAware_03_wc15_wr1.py
+++ configs/softIoU/MaskAwareIoU/RPN/rpn_r50_fpn_MaskAware_03_wc15_wr1.py
@@ -1,12 +1,10 @@
 # model settings
 model = dict(
-    type='RetinaNet',
-    pretrained='open-mmlab://resnext101_64x4d',
+    type='RPN',
+    pretrained='torchvision://resnet50',
     backbone=dict(
-        type='ResNeXt',
-        depth=101,
-        groups=64,
-        base_width=4,
+        type='ResNet',
+        depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
@@ -15,47 +13,46 @@ model = dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
-        start_level=1,
-        add_extra_convs=True,
         num_outs=5),
-    bbox_head=dict(
-        type='RetinaHead',
-        num_classes=81,
+    rpn_head=dict(
+        type='RPNHead',
         in_channels=256,
-        stacked_convs=4,
         feat_channels=256,
-        octave_base_scale=4,
-        scales_per_octave=3,
+        anchor_scales=[8],
         anchor_ratios=[0.5, 1.0, 2.0],
-        anchor_strides=[8, 16, 32, 64, 128],
-        target_means=[0., 0., 0., 0.],
-        target_stds=[1., 1., 1., 1.],
-        filename='./work_dirs/retinanet_x101_fpn_MaskAwareIoU/bg_fg.txt',        
+        anchor_strides=[4, 8, 16, 32, 64],
+        target_means=[.0, .0, .0, .0],
+        target_stds=[1.0, 1.0, 1.0, 1.0],
         loss_cls=dict(
-            type='FocalLoss',
-            use_sigmoid=True,
-            gamma=2.0,
-            alpha=0.25,
-            loss_weight=2.0),
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.5),
         loss_bbox=dict(type='L1Loss', loss_weight=1.0)))
-# training and testing settings
+# model training and testing settings
 train_cfg = dict(
-    assigner=dict(
-        type='MaxMaskAwareIoUAssigner',
-        pos_iou_thr=0.50,
-        neg_iou_thr=0.50,
-        maskIOUweight=0.25,
-        min_pos_iou=0,
-        ignore_iof_thr=-1),
-    allowed_border=-1,
-    pos_weight=-1,
-    debug=False)
+    rpn=dict(     
+        assigner=dict(
+            type='MaxMaskAwareIoUAssigner',
+            pos_iou_thr=0.7,
+            neg_iou_thr=0.3,
+            min_pos_iou=0.3,
+            maskIOUweight=0.25,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='RandomSampler',
+            num=256,
+            pos_fraction=0.5,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=False),
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False))
 test_cfg = dict(
-    nms_pre=1000,
-    min_bbox_size=0,
-    score_thr=0.05,
-    nms=dict(type='nms', iou_thr=0.5),
-    max_per_img=100)
+    rpn=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=2000,
+        max_num=2000,
+        nms_thr=0.7,
+        min_bbox_size=0))
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
@@ -63,13 +60,13 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='LoadAnnotations', with_bbox=True, with_label=False, with_mask=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_masks']),
 ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -105,9 +102,9 @@ data = dict(
         img_prefix=data_root + 'val2017/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+# runner configs
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
-# learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
@@ -125,10 +122,9 @@ log_config = dict(
 # yapf:enable
 # runtime settings
 total_epochs = 12
-device_ids = range(8)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/retinanet_x101_fpn_MaskAwareIoU'
+work_dir = './work_dirs/rpn_r50_fpn_MaskAware_03_wc15_wr1'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
