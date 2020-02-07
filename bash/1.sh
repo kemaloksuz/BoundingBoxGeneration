6b09681f8e6dc9a3a722bf75f94b15db334f3afe
Baris Can Cam <can.cam@metu.edu.tr>
2020-02-07 12:31:47 +0300

    sequential execution update.

--- bash/1.sh
+++ bash/1.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 CONFIG1="rpn_r50_fpn_MaskAware_03_wc1_wr1";
 CONFIG2="rpn_r50_fpn_MaskAware_03_wc15_wr1";
-mkdir -p "results/RPN/$CONFIG1" &&\
-python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" --gpus 4 &&\
-python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" "work_dirs/configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py/epoch12.pth"  —out "results/RPN/$CONFIG1/$CONFIG1.pkl" --eval proposal_fast >> "results/RPN/$CONFIG1/$CONFIG1.result" &&\
-mkdir -p "results/RPN/$CONFIG2" &&\
-python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" --gpus 4 &&\
+mkdir -p "results/RPN/$CONFIG1" &&
+python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" --gpus 4 &&
+python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" "work_dirs/configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py/epoch12.pth"  —out "results/RPN/$CONFIG1/$CONFIG1.pkl" --eval proposal_fast >> "results/RPN/$CONFIG1/$CONFIG1.result" &&
+mkdir -p "results/RPN/$CONFIG2" &&
+python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" --gpus 4 &&
 python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" "work_dirs/configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py/epoch12.pth"  —out "results/RPN/$CONFIG2/$CONFIG2.pkl" --eval proposal_fast >> "results/RPN/$CONFIG2/$CONFIG2.result"
