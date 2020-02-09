ceeac47e10d1d413898addd1977229b08f5dc502
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-02-09 17:42:48 +0100

    More RPN

--- bash/1.sh
+++ bash/1.sh
@@ -6,4 +6,4 @@ python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" --gpus 4 &&
 python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" "work_dirs/$CONFIG1/epoch12.pth" --out "results/RPN/$CONFIG1/$CONFIG1.pkl" --eval proposal_fast >> "results/RPN/$CONFIG1/$CONFIG1.result"
 mkdir -p "results/RPN/$CONFIG2" &&
 python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" --gpus 4 &&
-python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" "work_dirs/$CONFIG2/epoch12.pth"  —out "results/RPN/$CONFIG2/$CONFIG2.pkl" --eval proposal_fast >> "results/RPN/$CONFIG2/$CONFIG2.result"
+python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" "work_dirs/$CONFIG2/epoch12.pth"  --out "results/RPN/$CONFIG2/$CONFIG2.pkl" --eval proposal_fast >> "results/RPN/$CONFIG2/$CONFIG2.result"
---
+++ bash/10.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+CONFIG1="rpn_r50_fpn_MaskAware_05_wc15_wr15";
+mkdir -p "results/RPN/$CONFIG1" &&
+python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" --gpus 4 &&
+python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" "work_dirs/$CONFIG1/epoch12.pth"  --out "results/RPN/$CONFIG1/$CONFIG1.pkl" --eval proposal_fast >> "results/RPN/$CONFIG1/$CONFIG1.result"
