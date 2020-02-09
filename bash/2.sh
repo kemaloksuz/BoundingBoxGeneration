225e7d9a35bf534d9b7b4614623ea89aef9c25ba
cancam <bariscancam@gmail.com>
2020-02-09 12:26:43 +0000

    RPN bash fix.

--- bash/2.sh
+++ bash/2.sh
@@ -3,7 +3,7 @@ CONFIG1="rpn_r50_fpn_MaskAware_04_wc1_wr1";
 CONFIG2="rpn_r50_fpn_MaskAware_04_wc15_wr1";
 mkdir -p "results/RPN/$CONFIG1" &&
 python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" --gpus 4 &&
-python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" "work_dirs/$CONFIG1/epoch12.pth"  —out "results/RPN/$CONFIG1/$CONFIG1.pkl" --eval proposal_fast >> "results/RPN/$CONFIG1/$CONFIG1.result" &&
+python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" "work_dirs/$CONFIG1/epoch12.pth" --out "results/RPN/$CONFIG1/$CONFIG1.pkl" --eval proposal_fast >> "results/RPN/$CONFIG1/$CONFIG1.result"
 mkdir -p "results/RPN/$CONFIG2" &&
 python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" --gpus 4 &&
-python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" "work_dirs/$CONFIG2/epoch12.pth"  —out "results/RPN/$CONFIG2/$CONFIG2.pkl" --eval proposal_fast >> "results/RPN/$CONFIG2/$CONFIG2.result"
+python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG2.py" "work_dirs/$CONFIG2/epoch12.pth"  --out "results/RPN/$CONFIG2/$CONFIG2.pkl" --eval proposal_fast >> "results/RPN/$CONFIG2/$CONFIG2.result"
