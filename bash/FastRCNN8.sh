194374b716dbbba21f5eda3a8b1d3a5263ac9391
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-02-16 14:38:24 +0200

    configs

---
+++ bash/FastRCNN8.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+CONFIG1="fast_rcnn_r50_fpn_MaskAware_wc1_wr2";
+mkdir -p "results/FastRCNN/$CONFIG1"&&
+python tools/train.py "configs/softIoU/MaskAwareIoU/FastRCNN/$CONFIG1.py" --gpus 4 &&
+python tools/test.py "configs/softIoU/MaskAwareIoU/FastRCNN/$CONFIG1.py" "work_dirs/$CONFIG1/epoch_12.pth"  --json_out "results/FastRCNN/$CONFIG1/$CONFIG1" --eval bbox &&
+python tools/coco_eval.py "results/FastRCNN/$CONFIG1/$CONFIG1.bbox.json" --ann "data/coco/annotations/instances_val2017.json" --LRPEval 1 --LRPtau 0.5 >> "results/FastRCNN/$CONFIG1/$CONFIG1.result"
