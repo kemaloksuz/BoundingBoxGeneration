ff10ebd0dd16773e876857684d71a4dfc1f3dc92
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-03-01 14:27:50 +0200

    RPN OHEM

---
+++ bash/26.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+CONFIG1="rpn_r50_fpn_OHEM";
+mkdir -p "results/RPN/$CONFIG1" &&
+python tools/train.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" --gpus 4 &&
+python tools/test.py "configs/softIoU/MaskAwareIoU/RPN/$CONFIG1.py" "work_dirs/$CONFIG1/epoch_12.pth"  --out "results/RPN/$CONFIG1/$CONFIG1.pkl" --eval proposal_fast >> "results/RPN/$CONFIG1/$CONFIG1.result"
