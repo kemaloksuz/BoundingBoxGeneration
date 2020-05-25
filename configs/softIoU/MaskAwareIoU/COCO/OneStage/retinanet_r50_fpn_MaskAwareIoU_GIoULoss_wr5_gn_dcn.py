fec5a904c639bc6c225a0b60dc09cff3fa60a586
Kemal Oksuz <kemal.oksuz@metu.edu.tr>
2020-05-25 16:50:19 +0300

    Update retinanet_r50_fpn_MaskAwareIoU_GIoULoss_wr5_gn_dcn.py

--- configs/softIoU/MaskAwareIoU/COCO/OneStage/retinanet_r50_fpn_MaskAwareIoU_GIoULoss_wr5_gn_dcn.py
+++ configs/softIoU/MaskAwareIoU/COCO/OneStage/retinanet_r50_fpn_MaskAwareIoU_GIoULoss_wr5_gn_dcn.py
@@ -10,7 +10,7 @@ model = dict(
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
         style='pytorch',
-        norm_cfg=norm_cfg
+        norm_cfg=norm_cfg,
         dcn=dict(type='DCN', deformable_groups=1, fallback_on_stride=False),
         stage_with_dcn=(False, True, True, True)),
     neck=dict(
